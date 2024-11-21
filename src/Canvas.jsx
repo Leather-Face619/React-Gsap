b76ba3b523855926175357431e74d689bfb012e6
Leather-Face619 <rahul852369hhhhh@gmail.com>
2024-11-21 19:38:30 +0530

    2nd commit

--- src/Canvas.jsx
+++ src/Canvas.jsx
@@ -1,19 +1,36 @@
-import React, { useEffect, useRef } from 'react'
 import images from './images'
+import { useGSAP } from '@gsap/react'
+import { gsap } from 'gsap';
+import { useEffect, useRef, useState } from 'react';
+
 const Canvas = () => {
-    const canvasRef = useRef(null)
-    useEffect(() => {
+  const [index, setIndex] = useState({value: 0})
+  const canvasRef = useRef(null)
+
+  useGSAP(() => {
+    gsap.to(index, {
+      value: 147,
+      duration: 3,
+      repeat: -1,
+      ease: "linear",
+      onUpdate: () => {
+        setIndex({value: Math.round(index.value)})
+      },
+    })
+  })
+
+  useEffect(() => {
     const canvas = canvasRef.current
     const ctx = canvas.getContext('2d');
     const img = new Image();
-    img.onload = function() {
-      ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+    img.src = images[index.value];
+    img.onload = () => {
+      canvas.width = img.width;
+      canvas.height = img.height;
+      ctx.drawImage(img, 0, 0);
     };
-    img.src = images[20];
-    
-      
-    }, [])
-    
+  }, [index])
+
   return (
     <canvas ref={canvasRef} id="canvas" width="300" height="300"></canvas>
   )
